c0c0ec2cf7d128e2575dea3e748a485aa6cc54b4
utuedey <utuedeyejosph@gmail.com>
2025-08-18 21:42:34 +0100

    feat: add validation for user registration and login, integrate express-validator, and update routes

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -6,10 +6,6 @@ exports.register = catchAsync(async (req, res, next) => {
 
     const { name, email, password, role } = req.body;
 
-    // Basic validation
-    if (!name || !email || !password || !role) {
-        return next(new AppError('Please provide name, email, password, and role.', 400));
-    }
     const {user, token} = await authService.registerUser(name, email, password, role);
 
     res.status(201).json({
@@ -23,10 +19,6 @@ exports.register = catchAsync(async (req, res, next) => {
 
 exports.login = catchAsync(async (req, res, next) => {
     const { email, password } = req.body;
-
-    if (!email || !password){
-        return next(new AppError('Please provide email and password.', 400));
-    }
 
     const { token } = await authService.loginUser(email, password);
 
